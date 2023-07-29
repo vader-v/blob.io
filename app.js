94ef06c2dddc67af4d09bda6029086fe43cc0593
amirb <amir.bullock9@gmail.com>
2023-07-29 08:52:17 -0400

    add camera movement and gradual blob sizing dynamics and grid background

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 let blob;
 
 let blobs = [];
+let zoom = 1;
 
 function setup() {
   createCanvas(600, 600);
-  blob = new Blob(width/2, height/2, 64);
-  for (let i = 0; i < 50; i++) {
-    let x = random(-width, width*2);
-    let y = random(-height, height*2);
+  blob = new Blob(0, 0, 64);
+  for (let i = 0; i < 200; i++) {
+    let x = random(-width, width);
+    let y = random(-height, height);
     blobs[i] = new Blob(x, y, 16);
+  }
+}
+
+const gridSpacing = 20;
+
+function drawGrid() {
+  stroke(40); // Set the color of the grid lines
+  strokeWeight(1);
+
+  // Draw vertical lines
+  for (let x = -width; x <= width; x += gridSpacing) {
+    line(x, -height, x, height);
+  }
+
+  // Draw horizontal lines
+  for (let y = -height; y <= height; y += gridSpacing) {
+    line(-width, y, width, y);
   }
 }
 
 function draw() {
   background(0);
 
-  translate (-blob.pos.x + width/2, -blob.pos.y + height/2);
   
+  // translate (width / 2 - blob.pos.x, height / 2 - blob.pos.y);
+  translate(width / 2, height / 2);
+  let newscale = 64 / blob.r;
+  zoom = lerp(zoom, newscale, 0.2);
+  scale(zoom);
+  translate(-blob.pos.x, -blob.pos.y);
+  
+  drawGrid();
+
   for (let i = blobs.length-1; i >= 0; i--) {
     blobs[i].show();
     if (blob.eats(blobs[i])) {
--- blob.js
+++ blob.js
@@ -2,18 +2,22 @@ class Blob {
   constructor(x, y, r) {
     this.pos = createVector(x, y);
     this.r = r;
+    this.vel = createVector(0, 0);
 
     this.update = function() {
-      let vel = createVector(mouseX-width/2, mouseY-height/2);
-      vel.setMag(3); 
-      this.pos.add(vel);
+      let newvel = createVector(mouseX-width/2, mouseY-height/2);
+      newvel.setMag(3);
+      this.vel.lerp(newvel, 0.1);
+      this.pos.add(this.vel);
 
     }
 
     this.eats = function(other) {
       let d = p5.Vector.dist(this.pos, other.pos);
       if (d < this.r + other.r) { 
-        this.r += other.r * 0.2;
+        let sum = PI * this.r * this.r + PI * other.r * other.r;
+        this.r = sqrt(sum / PI);
+        // this.r += other.r;
         return true;
       } else {
         return false;
